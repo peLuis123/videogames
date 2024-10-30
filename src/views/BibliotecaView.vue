b30a1618f3383dbf655aac70e2932f6a7d647335
peLuis123 <pedrorc2018@gmail.com>
2024-10-29 23:36:23 -0500

    feat: library view

---
+++ src/views/BibliotecaView.vue
@@ -0,0 +1,15 @@
+<template >
+  <v-container>
+    <h1 class="text-center mb-5">Mi Biblioteca</h1>
+    <BibliotecaComponent />
+  </v-container>
+</template>
+<script setup>
+import BibliotecaComponent from "@/components/BibliotecaComponent.vue";
+</script>
+
+<style scoped>
+.title-row {
+  margin: 20px;
+}
+</style>
